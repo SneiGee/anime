4cd0caa5ab06f226f9cc8ddae415c9841f51404e
Michael Schneider <schneiderm312@gmail.com>
2024-07-03 13:30:41 +0000

    Commit

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,48 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import AnimeCard from "./AnimeCard";
+import { fetchAnime } from "@/app/action";
+
+let page = 2;
+
+export type AnimeCard = JSX.Element;
 
 function LoadMore() {
+  const { ref, inView } = useInView();
+
+  const [data, setData] = useState<AnimeCard[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (inView) {
+      setIsLoading(true);
+      // Add a delay of 500 milliseconds
+      const delay = 500;
+
+      const timeOutId = setTimeout(() => {
+        fetchAnime(page).then((res) => {
+          setData([...data, ...res]);
+          page++;
+        });
+
+        setIsLoading(false);
+      }, delay);
+
+      // Clear the timeout if the component is unmounted or inView becomes false
+      return () => clearTimeout(timeOutId);
+    }
+  }, [inView, data, isLoading]);
+
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data}
+      </section>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
